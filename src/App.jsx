6cdcd56b531d36d9a4070bd45a818a7a1e6e8062
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-12-12 21:28:53 +0530

    code updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Navbar from "./components/layout/Navbar"
-import Home from "./pages/Home/Home";
+import Home from "./pages/Home/Home"
 
 const App = () => {
     return (
         <>
             <Navbar />
 
+            {/* Main Page */}
             <Home />
         </>
     )
