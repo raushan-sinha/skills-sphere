3e08a94e3b0e0a797c0a0fd193e8eafab19da506
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-12-16 11:44:41 +0530

    Deleted files from Home Page

--- src/pages/Home/sections/index.js
+++
@@ -1,7 +0,0 @@
-export { default as HeroSection } from './HeroSection';
-export { default as AboutSection } from './AboutSection';
-export { default as ProjectSection } from './ProjectSection';
-export { default as BlogSection } from './BlogSection';
-export { default as SystemDesignSection } from './SystemDesignSection';
-export { default as CodingCollaborationSection } from './CodingCollaborationSection';
-export { default as HelpDeskSection } from './HelpDeskSection';
