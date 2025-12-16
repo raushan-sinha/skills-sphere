3e598c617fba9958bfd9f08ecfc416a1f78716b0
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-12-16 11:55:33 +0530

    Code updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar"
-import Home from "./pages/Home/Home"
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 
 const App = () => {
     return (
         <>
-            <header>
+            <BrowserRouter>
                 <Navbar />
-            </header>
-
-            {/* Main Page */}
-            <main>
-                <Home />
-            </main>
+                
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                </Routes>
+            </BrowserRouter>
 
             <Footer />
         </>
