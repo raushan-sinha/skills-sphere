01c2f1c642e374fb1671d59d66c3731d43b790f2
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-12-17 16:25:35 +0530

    Added Helmet with a title

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import HeroSection from './sections/HeroSection';
+import { Helmet } from 'react-helmet-async';
 
 export default function Home() {
     return (
-        <main>
-            <HeroSection />
-        </main>
+        <>
+            <Helmet>
+                <title>
+                    SkillSphere – Frontend Engineering, React & Next.js Insights by Raushan
+                </title>
+
+                <meta
+                    name="description"
+                    content="SkillSphere is a frontend engineering hub by Raushan featuring React, Next.js, system design docs, coding problems, tech blogs, and career guidance."
+                />
+            </Helmet>
+
+            <main>
+                <HeroSection />
+            </main>
+        </>
     );
 }
