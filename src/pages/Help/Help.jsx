70abc24aefc94ca0dd6ce71df1844fe0fb5b260d
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-12-19 11:35:07 +0530

    Created new file for Help Page

---
+++ src/pages/Help/Help.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Help = () => {
+  return (
+    <>
+        <section>
+            <form action="">
+                <div>
+                    <label htmlFor="">Full Name:</label>
+                </div>
+                <div></div>
+                <div></div>
+            </form>
+        </section>
+    </>
+  )
+}
+
+export default Help;
