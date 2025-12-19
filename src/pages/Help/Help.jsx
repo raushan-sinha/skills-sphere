fd6dd4a33c51ebbbacf1be20361506e680e11bb3
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-12-19 16:29:52 +0530

    Updated new code

--- src/pages/Help/Help.jsx
+++ src/pages/Help/Help.jsx
@@ -1,19 +1,12 @@
 import React from 'react'
+import HelpDeskForm from './HelpDeskForm';
 
 const Help = () => {
-  return (
-    <>
-        <section>
-            <form action="">
-                <div>
-                    <label htmlFor="">Full Name:</label>
-                </div>
-                <div></div>
-                <div></div>
-            </form>
+    return (
+        <section className="relative min-h-screen flex items-center justify-center overflow-hidden bg-gradient-to-br from-indigo-900 via-purple-900 to-black px-4 py-20">
+            <HelpDeskForm />
         </section>
-    </>
-  )
+    )
 }
 
 export default Help;
