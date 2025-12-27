e37d1ff104b5e8be33ce1ea8820083704f66ac79
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-12-27 20:39:08 +0530

    added code for submitting, error message, success message logic.

--- src/pages/Help/HelpDeskForm.jsx
+++ src/pages/Help/HelpDeskForm.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { HELPVALIDATION } from '../../utils/helpValidation';
+import { Helmet } from 'react-helmet-async';
+import { HELPSERVICEAPI } from '../../services/helpService';
 
 const HelpDeskForm = () => {
+    //todo: Add auto focus in input field -
     const inputRef = useRef(null);
     useEffect(() => {
         inputRef.current.focus();
@@ -9,28 +12,58 @@ const HelpDeskForm = () => {
 
     //todo: Create a logic for Form input validations after submitting -
     const [error, setError] = useState({});
+    const [successMsg, setSuccessMsg] = useState('');
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
-        const formData = new FormData(e.target);
+        const form = e.currentTarget;
+        const formData = new FormData(form);
         const data = Object.fromEntries(formData.entries());
         const validationErrors = HELPVALIDATION(data);
         setError(validationErrors);
 
-        if (Object.keys(validationErrors).length === 0) {
-            console.log('Form is valid', data);
+        if (Object.keys(validationErrors).length !== 0) {
+            return;
+        }
+
+        try {
+            await HELPSERVICEAPI(data);
+            setSuccessMsg('Message sent successfully.');
+            setTimeout(() => {
+                setSuccessMsg('')
+            }, 3000);
+            form.reset();
+        } catch (error) {
+            setError({ api: error.message || 'Network connection is poor' });
         }
     };
 
-    //todo: Reset data -
-    const resetData = () => {
-        setError({});
+    //todo: Submit form by the key -
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter' && !e.shiftKey) {
+            const form  = e.currentTarget.form;
+            if (form) {
+                form.requestSubmit();
+            }
+        }
     }
-
 
     return (
         <>
+            <Helmet>
+                <title>Help | SkillSphere</title>
+                <meta
+                    name="description"
+                    content="In this Help page, you can contact or take help with me. You can share your coding issues, this website feedback and anything."
+                />
+                <meta
+                    name="keywords"
+                    content="SkillSphere, about SkillSphere, developer platform, frontend learning, tech community, developer growth"
+                />
+                <meta name="author" content="SkillSphere" />
+            </Helmet>
+
             {/* Card */}
             <div className="relative w-full max-w-md sm:max-w-lg backdrop-blur-xl bg-white/10 border border-white/20 rounded-2xl shadow-2xl p-5 sm:p-8 animate-fadeIn mt-4">
                 {/* Header */}
@@ -103,18 +136,12 @@ const HelpDeskForm = () => {
                             name='message'
                             autoComplete='off'
                             placeholder="Send Message...."
+                            onKeyDown={handleKeyDown}
                             className="w-full rounded-lg bg-white/20 border border-white/30 px-3 py-2.5 text-sm text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-500 transition resize-none" />
                     </div>
 
                     {/* Buttons */}
-                    <div className="flex flex-col sm:flex-row gap-3 pt-3">
-                        <button
-                            type='reset'
-                            onClick={resetData}
-                            className="w-full sm:w-1/2 rounded-lg border border-white/30 py-2.5 text-sm text-white font-medium hover:bg-white/10 transition cursor-pointer">
-                            Reset
-                        </button>
-
+                    <div className="flex justify-center">
                         <button
                             type="submit"
                             className="w-full sm:w-1/2 rounded-lg bg-gradient-to-r from-purple-600 to-indigo-600 py-2.5 text-sm text-white font-semibold hover:opacity-90 transition cursor-pointer">
@@ -122,6 +149,12 @@ const HelpDeskForm = () => {
                         </button>
                     </div>
                 </form>
+                <br />
+                <div className="flex justify-center items-center">
+                    <span className="text-cyan-600 font-bold text-xl">
+                        {successMsg}
+                    </span>
+                </div>
             </div>
         </>
     );
