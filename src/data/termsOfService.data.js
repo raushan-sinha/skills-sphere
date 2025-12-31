f0a7daeb69c79b11544424a081b08d5d4ad26576
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-12-31 21:09:22 +0530

    created a file for stored data

---
+++ src/data/termsOfService.data.js
@@ -0,0 +1,38 @@
+export const termsOfService = [
+    {
+        title: 'Acceptance of Terms',
+        para: 'By accessing or using SkillSphere, you agree to comply with and be bound by these Terms of Service. If you do not agree, you should discontinue use of the platform immediately.'
+    },
+    {
+        title: 'About SkillSphere',
+        para: 'SkillSphere is a technology-focused learning and collaboration platform offering tech blogs, resources, project collaboration opportunities, and AI-powered tools. Our goal is to empower developers, learners, and innovators globally.'
+    },
+    {
+        title: 'Intellectual Property',
+        para: 'All content, branding, UI elements, and original materials on SkillSphere are the intellectual property of SkillSphere unless stated otherwise. Unauthorized reproduction or redistribution is strictly prohibited.'
+    },
+    {
+        title: 'User-Generated Content',
+        para: 'You retain ownership of any content you submit. However, by posting on SkillSphere, you grant us a non-exclusive, royalty-free license to display and distribute your content within the platform for operational and promotional purposes.'
+    },
+    {
+        title: 'Third-Party Links',
+        para: 'SkillSphere may include links to third-party websites or services. We are not responsible for the content, policies, or practices of any external platforms.'
+    },
+    {
+        title: 'Termination of Access',
+        para: ' We reserve the right to suspend or terminate access to SkillSphere at any time if a user violates these terms or engages in harmful behavior.'
+    },
+    {
+        title: 'Disclaimer & Limitation of Liability',
+        para: 'SkillSphere is provided on an “as-is” basis. We do not guarantee uninterrupted availability or error-free performance. To the fullest extent permitted by law, SkillSphere shall not be liable for any indirect or consequential damages.'
+    },
+    {
+        title: 'Changes to These Terms',
+        para: 'We may update these Terms of Service periodically. Continued use of SkillSphere after changes implies acceptance of the revised terms.'
+    },
+    {
+        title: 'Contact Information',
+        para: 'If you have questions or concerns regarding these Terms, you can reach out via the official SkillSphere communication channels.'
+    },
+];
