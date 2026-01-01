b5c9f7910b43f922c26bf0235663b30e24408511
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-01-01 20:23:50 +0530

    Updated padding

--- src/pages/Help/Help.jsx
+++ src/pages/Help/Help.jsx
@@ -3,7 +3,7 @@ import HelpDeskForm from './HelpDeskForm';
 
 const Help = () => {
     return (
-        <section className="relative min-h-screen flex items-center justify-center overflow-hidden bg-gradient-to-br from-indigo-900 via-purple-900 to-black px-4 py-20">
+        <section className="relative min-h-screen flex items-center justify-center overflow-hidden bg-gradient-to-br from-indigo-900 via-purple-900 to-black px-4 py-25">
             <HelpDeskForm />
         </section>
     )
